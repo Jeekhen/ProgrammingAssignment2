9741d38febd2e9f3fe02b1e19baefe4aa02e921c
Wong Jee Khen <jeekhen@yahoo.com>
2016-04-11 09:04:49 +0800

    - Added functionality to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_matrix <<- NULL
+        
+        set <- function(new_matrix) {
+                norm_matrix <<- new_matrix
+                inv_matrix <<- NULL
+        }
+        get <- function () x
+        setInverseMatrix <- function(new_matrix2) inv_matrix <<- new_matrix2
+        getInverseMatrix <- function() inv_matrix
+        list(set = set, get = get,
+             setInverseMatrix = setInverseMatrix,
+             getInverseMatrix = getInverseMatrix)
 }
 
 
@@ -12,4 +23,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        result <- x$getInverseMatrix()
+        
+        if(!is.null(result)){
+                message("Cached data was retrieve")
+        }else{
+                data <- x$get()
+                result <- solve(data)
+                x$setInverseMatrix(result)
+        }
+        
+        result 
 }
